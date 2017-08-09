ec285932c39f3cb392fe69a2f16d617a85cd1b2e
Renan Le Gall <renan.legall@ieseg.fr>
2017-08-09 11:27:27 +0200

    rails new and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+    resources :restaurants do
+      collection do
+        get 'top', to: 'restaurants#top'
+      end
+      member do
+        get 'chef', to: 'restaurants#chef'
+      end
+      resources :reviews, shallow: :true
+    end
+    namespace :admin do
+      resources :restaurants, only: :index
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
